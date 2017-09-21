d767864e36bea918350dd742846cf2cc095f3559
Chad Garrett <chadgarrett_@hotmail.com>
2017-09-21 14:32:13 -0700

    Added additional comments

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ app.use(express.static("public"));
 
 // Connect to MongoDB
 MongoClient.connect('mongodb://localhost:27017/tweeter', (err, db) => {
+
   if (err) {
     console.log(`Failed to connect to mongodb`);
     throw err;
@@ -21,14 +22,17 @@ MongoClient.connect('mongodb://localhost:27017/tweeter', (err, db) => {
   // We have a connection starting here.
   console.log(`Connected to mongodb`);
 
+  // /users Routing and DataHelpers
   const usersDataHelpers = require("./lib/users-data-helpers.js")(db);
   const usersRoutes = require("./routes/users")(usersDataHelpers);
   app.use("/users", usersRoutes);
 
+  // /tweets Routing and DataHelpers
   const tweetsDataHelpers = require("./lib/tweets-data-helpers.js")(db);
   const tweetsRoutes = require("./routes/tweets")(tweetsDataHelpers);
   app.use("/tweets", tweetsRoutes);
 
+  // /login Routing and DataHelpers
   const loginRoutes = require("./routes/login")(usersDataHelpers);
   app.use("/login", loginRoutes);
 
