5d200b322beada5dfe0ee9e2067f6c9421f18f6d
Chad Garrett <chadgarrett_@hotmail.com>
2017-09-21 14:12:39 -0700

    Created verifyUserLogin function to verify against DB

--- server/lib/users-data-helpers.js
+++ server/lib/users-data-helpers.js
@@ -1,17 +1,32 @@
 "use strict";
 
-// Defines helper functions for saving and getting tweets, using the database `db`
+const md5 = require('md5');
+
+// Defines helper functions for saving, getting, and verifying users, using the database `db`
 module.exports = function userDataHelpers(db) {
   return {
 
-    saveUser: function(newUser, callback) {
+    saveUser: function (newUser, callback) {
       db.collection("users").insertOne(newUser, (err, result) => {
-          callback(err, result);
+        callback(err, result);
       });
     },
 
-    getUsers: function(callback) {
-        db.collection("users").find().sort({created_at: -1}).toArray(callback);        
+    getUsers: function (callback) {
+      db.collection("users").find().sort({ created_at: -1 }).toArray(callback);
+    },
+
+    verifyUserLogin: function (userHandle, userPassword, callback) {
+      db.collection("users").find({handle: userHandle, password: md5(userPassword)}).toArray((err, documents) => {
+
+        if (documents.length === 0) {
+          const err = { message: 'No user found using those credentials' }
+          callback(err, null);
+          return;
+        }
+
+        callback(null, documents);
+      });
     }
 
   };
