6559c9be7f2ba8738f1019d60015b47b82976363
Chad Garrett <chadgarrett_@hotmail.com>
2017-09-21 11:51:21 -0700

    Removed old comments, added routing info for /users

--- server/index.js
+++ server/index.js
@@ -11,31 +11,22 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// The in-memory database of tweets. It's a basic object with an array in it.
-// const db = require("./lib/in-memory-db");
+// Connect to MongoDB
 MongoClient.connect('mongodb://localhost:27017/tweeter', (err, db) => {
   if (err) {
     console.log(`Failed to connect to mongodb`);
     throw err;
   }
 
-  // We have a connection to the "tweeter" db, starting here.
+  // We have a connection starting here.
   console.log(`Connected to mongodb`);
 
-  // The `data-helpers` module provides an interface to the database of tweets.
-  // This simple interface layer has a big benefit: we could switch out the
-  // actual database it uses and see little to no changes elsewhere in the code
-  // (hint hint).
-  //
-  // Because it exports a function that expects the `db` as a parameter, we can
-  // require it and pass the `db` parameter immediately:
-  const DataHelpers = require("./lib/data-helpers.js")(db);
+  const usersDataHelpers = require("./lib/users-data-helpers.js")(db);
+  const usersRoutes = require("./routes/users")(usersDataHelpers);
+  app.use("/users", usersRoutes);
 
-  // The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
-  // so it can define routes that use it to interact with the data layer.
-  const tweetsRoutes = require("./routes/tweets")(DataHelpers);
-
-  // Mount the tweets routes at the "/tweets" path prefix:
+  const tweetsDataHelpers = require("./lib/tweets-data-helpers.js")(db);
+  const tweetsRoutes = require("./routes/tweets")(tweetsDataHelpers);
   app.use("/tweets", tweetsRoutes);
 });
 
